0d0656add494aaf3761e2dae4a4937b9852373b4
Darren-Haynes <darrenhaynes@zoho.com>
2017-10-25 14:04:48 -0700

    added setup.py and tox.ini and restructured

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='http-server',
+    description='Build an echo server.',
+    package_dir={'': 'src'},
+    author='Chai Nareulla & Darren Haynes',
+    author_email='dummmy-address@zoho.com',
+    py_modules=[],
+    install_requires=[],
+    extras_require={
+        'testing': ['pytest', 'pytest-cov', 'pytest-watch', 'tox'],
+        'development': ['ipython']
+    },
+    entry_points={
+        # 'console_scripts': {
+            # 'http-server=http-server:main'
+        # }
+    }
+)
