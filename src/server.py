cfb3fd5d5c783ddc561247093fe999eb4f9ff969
Darren-Haynes <darrenhaynes@zoho.com>
2017-10-27 15:02:27 -0700

    return proper http status codes

--- src/server.py
+++ src/server.py
@@ -2,38 +2,44 @@
 """Open a server and listen for messages from the client."""
 import socket
 import sys
+from requests import HTTPError
 
 
-def response_ok():
+def response_ok(URI):
     """Return a successfull http 200 response."""
-    return b'HTTP/1.1 200 OK\r\n'
+    byte_URI = str(URI).encode('UTF8')
+    print("BYTE URI")
+    print(byte_URI)
+    return byte_URI + " " + b'HTTP/1.1 200 OK\r\n'
 
 
 def response_error(err):
     """Return http 500 response error."""
-    return b'err' + b' HTTP/1.1 500 OK\r\n'
+    return err + b' HTTP/1.1 500 OK\r\n'
 
 
 def parse_request(request):
     """Parse request to make sure it is a GET request."""
+    print("THIS IS REQUEST")
+    print(type(request))
     print(request)
     if b"GET" not in request:
-        raise ValueError("Server Only accepting GET requests.")
+        raise ValueError("405 error: only GET method accepted")
     elif b"HTTP/1.1" not in request:
-        raise ValueError("HTTP Request is not version 1.1.")
-    elif b"HOST: 127.0.0.1:5000" not in request:
-        raise ValueError("Bad Request: No Host ")
-    elif b"GET /http-server/src/server.py HTTP/1.1\r\n" not in request:
-        raise ValueError("Malformed-Request")
+        raise ValueError("505 error: HTTP Request is not version 1.1.")
+    elif b"Host: 127.0.0.1:5000" not in request:
+        raise ValueError("400 error: Bad Request")
+    elif b"GET /http-server/src/server.py HTTP/1.1 200\r\n" not in request:
+        raise ValueError("400: Malformed-Request")
     else:
-        return request.split(" ")[1]
+        return str(request).split(" ")[1]
 
 
 def server():  # pragma no cover
     """Open a server to echo back a message."""
     server = socket.socket(socket.AF_INET,
                            socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    server.bind(('127.0.0.1', 5011))
+    server.bind(('127.0.0.1', 5000))
     server.listen(1)
     try:
         while True:
@@ -46,11 +52,11 @@ def server():  # pragma no cover
                 if b"@@" in msg:
                     timer = False
             try:
-                parse_request(msg)
-                conn.sendall(response_ok())
+                URI = parse_request(msg)
+                conn.sendall(response_ok(URI))
             except ValueError as err:
-                print(err)
-                conn.sendall(response_error(err))
+                byte_err = str(err).encode('UTF8')
+                conn.sendall(response_error(byte_err))
             conn.close()
     except KeyboardInterrupt:
         conn.close()
