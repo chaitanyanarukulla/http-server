288146074a77d87d7da3721181063699ebe174e7
Darren-Haynes <darrenhaynes@zoho.com>
2017-11-02 20:19:00 -0700

    added appropriate doc strings

--- src/test_servers.py
+++ src/test_servers.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Test server"""
+"""Test server."""
 import pytest
 
 
@@ -12,14 +12,14 @@ def test_response_200():
 
 
 def test_response_error():
-    """Test server responds with http 500 response"""
+    """Test server responds with http 500 response."""
     from server import response_error
     err = b"405 error: only GET method accepted"
     assert err + b' HTTP/1.1 500 OK\r\n' == response_error(err)
 
 
 def test_parse_function_raises_value_error_with_405():
-    """Read function name"""
+    """Test appropriate 405 error when 'GET" not passed."""
     from server import parse_request
     with pytest.raises(ValueError,
                        match='405 error: only GET method accepted'):
@@ -27,7 +27,7 @@ def test_parse_function_raises_value_error_with_405():
 
 
 def test_parse_function_raises_value_error_with_505():
-    """Read function name"""
+    """Test appropriate 505 error when 'HTTP' not in string."""
     from server import parse_request
     with pytest.raises(ValueError,
                        match='505 error: HTTP Request is not version 1.1'):
@@ -35,21 +35,21 @@ def test_parse_function_raises_value_error_with_505():
 
 
 def test_parse_function_raises_value_error_with_400_bad_request():
-    """Read function name"""
+    """Test appropriate 400 error raised with bad request."""
     from server import parse_request
     with pytest.raises(ValueError, match='400 error: Bad Request'):
         parse_request('GET HTTP/1.1 potato')
 
 
 def test_parse_function_raises_value_error_with_400_malformed_request():
-    """Read function name"""
+    """Test appropriate 400 error raised with malformed request."""
     from server import parse_request
     with pytest.raises(ValueError, match='400: Malformed-Request'):
         parse_request('GET HTTP/1.1 Host: 127.0.0.1:5000 potato')
 
 
 def test_parse_function_returns_uri():
-    """Check function raises no errors and returns uri"""
+    """Check function raises no errors and returns uri."""
     from server import parse_request
     header = "GET /http-server/src/server.py HTTP/1.1 200\r\n" \
              "Host: 127.0.0.1:5000\r\n"
